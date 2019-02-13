8c31930ad6572b682efdd1261ce59ae45e381664
ramonsq <chimpin3000@hotmail.es>
2019-02-14 00:48:52 +0100

    fixed an error

--- session-7/exercise-2.py
+++ session-7/exercise-2.py
@@ -10,15 +10,3 @@ PORT = 8083
 IP = "212.128.253.64"
 
 file = input("Type a message:")
-# Connect to the server
-s.connect((IP, PORT))
-
-s.send(str.encode(file))
-
-msg = s.recv(2048).decode("utf-8")
-print("MESSAGE FROM THE SERVER")
-print(msg)
-
-s.close()
-
-print("The end")
