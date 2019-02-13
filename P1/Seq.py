1de5b4093922bbe1b12edde18b1981ac002a67aa
ramonsq <chimpin3000@hotmail.es>
2019-02-13 12:05:07 +0100

    commit for P1 in my new repository

---
+++ P1/Seq.py
@@ -0,0 +1,30 @@
+class Seq:
+
+    def __init__(self, strbases):
+        self.strbases = strbases
+
+    def len(self):
+        return len(self.strbases)
+
+    def complement(self):
+        comp_bases = {"A": "T", "T": "A", "C": "G", "G": "T"}
+        translation = self.strbases.maketrans(comp_bases)
+        strbases_comp = self.strbases.translate(translation)
+
+    def reverse(self):
+        strbases_rev = self.strbases[::-1]
+        return Seq(strbases_rev)
+
+    def count(self, base):
+        n_base = self.strbases.count(base)
+        return n_base
+
+    def perc(self, base):
+        total_length = len(self.strbases)
+        if total_length > 0:
+            nbase = self.count(base)
+            perc = round(100.0 * nbase / total_length, 1)
+        else:
+            perc = 0
+
+        return perc
