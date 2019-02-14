e145d074204df8510d7e4465d5e0f6d0822478e4
ramonsq <chimpin3000@hotmail.es>
2019-02-14 18:24:46 +0100

    P2 done!!!!!

--- P1/Seq_main.py
+++ P1/Seq_main.py
@@ -5,14 +5,13 @@ s2 = Seq("CGTAAC")
 s3 = s1.complement()
 s4 = s3.reverse()
 
-
 sequences = [s1, s2, s3, s4]
-
+counter = 1
 for elem in sequences:
-    print("Sequence:", elem.strbases)
+    print("Sequence {}:".format(counter), elem.strbases)
     print("  Length:", elem.len())
     bases = ["A", "T", "C", "G"]
-
+    counter += 1
     for i in bases:
         print("  Bases count: {}, {}".format(i, elem.count(i)))
         print("  Bases {} percentage: {}".format(i, elem.perc(i)), "%")
