8eb760a5b9db1aeed58ce4062a393cd3318ff2b4
ramonsq <chimpin3000@hotmail.es>
2019-02-16 21:15:31 +0100

    fixed an error

--- session-7/teacher-server.py
+++ session-7/teacher-server.py
@@ -33,7 +33,7 @@ try:
         msg = clientsocket.recv(2048).decode("utf-8")
         print("Message from client: {}".format(msg))
 
-        # Send the messag
+        # Send the message
         message = "Hello from the teacher's server"
         send_bytes = str.encode(message)
         # We must write bytes, not a string
