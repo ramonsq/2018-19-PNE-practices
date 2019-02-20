3f64fc1fc5322db9e3dfc603eaf706cffc6747ff
ramonsq <chimpin3000@hotmail.es>
2019-02-20 01:33:27 +0100

    p3

---
+++ P3/server.py
@@ -0,0 +1,42 @@
+import socket
+from Seq import Seq
+
+PORT = 8080
+IP = "192.168.56.1"
+MAX_OPEN_REQUEST = 5
+
+
+def process_client(cs):
+
+    # Reading the message from the client
+    msg = cs.recv(2048).decode("utf-8")
+
+    print("Message from the client: {}".format(msg))
+
+    # Sending the message back to the client
+    # (because we are an echo server)
+    cs.send(str.encode(msg))
+    if msg == "len":
+        print(msg.len())
+
+# Create a socket for connecting to the client
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+serversocket.bind((IP, PORT))
+
+serversocket.listen(MAX_OPEN_REQUEST)
+
+print("Socket ready: {}".format(serversocket))
+
+while True:
+
+    print("Waiting or connections at: {}, {}".format(IP, PORT))
+    (clientsocket, address) = serversocket.accept()
+
+    # -- Process the client request
+    print("Attending client: {}".format(address))
+
+    process_client(clientsocket)
+
+    # -- Process the client request
+    clientsocket.close()
