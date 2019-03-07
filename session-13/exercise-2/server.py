d4362a1178971b5cbe595b9fc8b50820177828d1
ramonsq <chimpin3000@hotmail.es>
2019-03-07 01:32:08 +0100

    i think it's done but i can't get it to work from the terminal, i will finish it on class

---
+++ session-13/exercise-2/server.py
@@ -0,0 +1,43 @@
+import http.server
+import socketserver
+
+PORT = 8000
+
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        print("GET received")
+
+        print("Request line:" + self.requestline)
+        print("  Cmd: " + self.command)
+        print("  Path: " + self.path)
+
+        content = ''
+        if self.path == '/':
+            with open('index.html', 'r') as file:
+                for line in file:
+                    content += line
+                    content = str(content)
+        else:
+            with open('error.html', 'r') as file:
+                for line in file:
+                    content += line
+                    content = str(content)
+
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/plain')
+        self.send_header("Content-Length", len(str.encode(content)))
+        self.end_headers()
+
+        self.wfile.write(str.encode(content))
+
+        return
+
+
+Handler = TestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Serving at PORT", PORT)
+
+    httpd.serve_forever()
