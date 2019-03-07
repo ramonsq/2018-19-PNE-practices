84ed5cc75ccd9929b4bd3537c4d818085b40bbfc
ramonsq <chimpin3000@hotmail.es>
2019-03-07 18:51:22 +0100

    p5

--- P5/webserver.py
+++ P5/webserver.py
@@ -1,39 +1,30 @@
-import socket
-import termcolor
+import http.server
+import socketserver
 
-# Change this IP to yours!!!!!
-IP = "212.128.253.90"
-PORT = 8089
-MAX_OPEN_REQUESTS = 57
+PORT = 8000
 
 
-def process_client(cs):
-    """Process the client request.
-    Parameters:  cs: socket for communicating with the client"""
+class TestHandler(http.server.BaseHTTPRequestHandler):
 
-    # Read client message. Decode it as a string
-    msg = cs.recv(2048).decode("utf-8")
+    def do_GET(self):
+        print("GET received")
 
-    # Print the received message, for debugging
-    if msg != '':
-        print()
-        print("Request message: ")
-        termcolor.cprint(msg, 'green')
-
-        request = msg.partition('\n')[0].split(' ')[1]
+        print("Request line:" + self.requestline)
+        print("  Cmd: " + self.command)
+        print("  Path: " + self.path)
 
         content = ''
-        if request == '/':
+        if self.path == '/':
             with open('index.html', 'r') as file:
                 for line in file:
                     content += line
                     content = str(content)
-        elif request == '/blue':
+        elif self.path == '/blue':
             with open('blue.html', 'r') as file:
                 for line in file:
                     content += line
                     content = str(content)
-        elif request == '/pink':
+        elif self.path == '/pink':
             with open('pink.html', 'r') as file:
                 for line in file:
                     content += line
@@ -44,43 +35,20 @@ def process_client(cs):
                     content += line
                     content = str(content)
 
-        status_line = "HTTP/1.1 200 ok\r\n"
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/plain')
+        self.send_header("Content-Length", len(str.encode(content)))
+        self.end_headers()
 
-        header = "Content-Type: text/html\r\n"
-        header += "Content-Length: {}\r\n".format(len(str.encode(content)))
+        self.wfile.write(str.encode(content))
 
-        response_msg = status_line + header + "\r\n" + content
-
-        cs.send(str.encode(response_msg))
-
-    else:
-        print("Your request is empty ):")
-    # Close the socket
-    cs.close()
+        return
 
 
-# MAIN PROGRAM
+Handler = TestHandler
 
-# create an INET, STREAMing socket
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Serving at PORT", PORT)
 
-# Bind the socket to the IP and PORT
-serversocket.bind((IP, PORT))
+    httpd.serve_forever()
 
-# Configure the server sockets
-# MAX_OPEN_REQUESTS connect requests before refusing outside connections
-serversocket.listen(MAX_OPEN_REQUESTS)
-
-print("Socket ready: {}".format(serversocket))
-
-while True:
-    # accept connections from outside
-    # The server is waiting for connections
-    print("Waiting for connections at {}, {} ".format(IP, PORT))
-    (clientsocket, address) = serversocket.accept()
-
-    # Connection received. A new socket is returned for communicating with the client
-    print("Attending connections from client: {}".format(address))
-
-    # Service the client
-    process_client(clientsocket)
