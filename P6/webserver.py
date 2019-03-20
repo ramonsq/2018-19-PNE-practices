f2c323d77e473a8af6892c496394bc9696a96615
ramonsq <chimpin3000@hotmail.es>
2019-03-20 19:05:54 +0100

    p6 almost done

--- P6/webserver.py
+++ P6/webserver.py
@@ -3,7 +3,7 @@ import socketserver
 import termcolor
 from Seq import Seq
 
-PORT = 8019
+PORT = 8009
 
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
@@ -13,31 +13,57 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         termcolor.cprint(self.requestline, 'green')
         print('Path: ' + self.path)
         path = self.path
-        i = path.find('=')
 
         if self.path == '/':
             with open("main.html", "r") as f:
                 contents = f.read()
             print('Path: ' + self.path)
+            resp = 200
             path = self.path
-            i = path.find('=')
+        elif path[:4] == '/myserver':
+            msg =path.split('&')
+            my_seq = msg[0].split('=')[1]
+            resp = 200
+            if my_seq.upper().strip('ACGT') == '':
+                my_seq = Seq(my_seq)
+
+                # i create new dics for the options
+                count = {'base=A': ('Count A: ' + my_seq.count('A')), 'base=C': ('Count C: ' + my_seq.count('C')), 'base=G': ('Count G: ' + my_seq.count('G')), 'base=T': ('Count T: ' + my_seq.count('T'))}
+                perc = {'base=A': ('Percentage of A: ' + my_seq.perc('A') + '%'), 'base=C': ('Percentage of C: ' + my_seq.perc('C') + '%'), 'base=T': ('Percentage of T: ' + my_seq.perc('T') + '%'), 'base=G': ('Percentage of G: ' + my_seq.perc('G') + '%')}
+
+                # now, i create another dic to place both previous dics
+                pos_ops = {'count': count, 'perc': perc}
+
+                # when you write no message you get a len of 3, so you have to star from there
+                if len(msg) == 3:
+                    # now a create an empty list with the length, so when i don choose it i get no answer from it
+                    l = ''
+                    operation = msg[1].split('=')[1]
+                    bases = msg[2]
+                    # i try to find things inside the dics with the function key, that introduces inside the dics
+                    if bases in pos_ops[operation].keys():
+                        bases_operation = pos_ops[operation][bases]
+
+                elif len(msg) == 4: # that means that i actually wrote a message
+                    l = 'Length: ' + str(my_seq.len())
+                    operation = msg[2].split('=')[1]
+                    bases = msg[3]
+                    if bases in pos_ops[operation].keys():
+                        bases_operations = pos_ops[operation][bases]
+                    contents = contents.replace('msg', msg)
+                else:
+                    with open ('seqerror.html', 'r') as f:
+                        contents = f.read()
+
         else:
+            resp = 404
             with open("error.html", "r") as f:
                 contents = f.read()
-        if i != -1:
-            if path.find("&") != -1:
-                ii = path.find("&")
-                msg = path[i + 1: ii]
-                msg = msg.len()
-            else:
-                msg = path[i + 1:]
-            with open('response.html', 'r') as f:
-                contents = f.read()
-            contents = contents.replace('msg', msg)
 
 
-        self.send_response(200)
 
+
+        self.send_response(resp)
         self.send_header('Content-Type', 'text/html')
         self.send_header('Content-Length', len(str.encode(contents)))
         self.end_headers()
