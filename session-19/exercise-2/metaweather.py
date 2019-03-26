a52402c6d9a74c9cb608fc366bcb77528205fae2
ramonsq <chimpin3000@hotmail.es>
2019-03-26 19:05:33 +0100

    session-19

--- session-19/exercise-2/metaweather.py
+++ session-19/exercise-2/metaweather.py
@@ -4,61 +4,92 @@
 import http.client
 import json
 
-# -- API information
+def get_woeid(capital):
+    try:
+        # -- API information
+        HOSTNAME = "www.metaweather.com"
+        ENDPOINT = "/api/location/search/?query="
+        ENDPOINT = ENDPOINT + capital
+
+        METHOD = "GET"
+
+        # -- Here we can define special headers if needed
+        headers = {'User-Agent': 'http-client'}
+
+        # -- Connect to the serversession-19/exercise-2/metaweather.py:18
+        # -- NOTICE it is an HTTPS connection!
+        # -- If we do not specify the port, the standar one
+        # -- will be used
+        conn = http.client.HTTPSConnection(HOSTNAME)
+
+        # -- Send the request. No body (None)
+        # -- Use the defined headers
+        conn.request(METHOD, ENDPOINT, None, headers)
+
+        # -- Wait for the server's response
+        r1 = conn.getresponse()
+
+        # -- Read the response's body and close
+        # -- the connection
+        text_json = r1.read().decode("utf-8")
+        conn.close()
+
+        index = json.loads(text_json)
+        return index[0]["woeid"]
+    except:
+        return False
+
 HOSTNAME = "www.metaweather.com"
-ENDPOINT = "/api/location/search/?query="
+ENDPOINT = "/api/location/"
+capital = input("Introduce a valid capital: ")
+if not (get_woeid(capital)):
+    print("Could not find that capital, please try again...")
+else:
+    LOCATION_WOEID = str(get_woeid(capital))
+    METHOD = "GET"
 
-# -- For the location we have to use the
-# -- Were on earth identifier
-# -- London woeid = 44418
-# -- Madrid woeid = 766273
+    # -- Here we can define special headers if needed
+    headers = {'User-Agent': 'http-client'}
 
-LOCATION_WOEID = input('Introduce a valid capital:', )
-METHOD = "GET"
+    # -- Connect to the server
+    # -- NOTICE it is an HTTPS connection!
+    # -- If we do not specify the port, the standar one
+    # -- will be used
+    conn = http.client.HTTPSConnection(HOSTNAME)
 
-# -- Here we can define special headers if needed
-headers = {'User-Agent': 'http-client'}
+    # -- Send the request. No body (None)
+    # -- Use the defined headers
+    conn.request(METHOD, ENDPOINT + LOCATION_WOEID + '/', None, headers)
 
-# -- Connect to the serversession-19/exercise-2/metaweather.py:18
-# -- NOTICE it is an HTTPS connection!
-# -- If we do not specify the port, the standar one
-# -- will be used
-conn = http.client.HTTPSConnection(HOSTNAME)
+    # -- Wait for the server's response
+    r1 = conn.getresponse()
 
-# -- Send the request. No body (None)
-# -- Use the defined headers
-conn.request(METHOD, ENDPOINT + LOCATION_WOEID + '/', None, headers)
+    # -- Print the status
+    print()
+    print("Response received: ", end='')
+    print(r1.status, r1.reason)
 
-# -- Wait for the server's response
-r1 = conn.getresponse()
+    # -- Read the response's body and close
+    # -- the connection
+    text_json = r1.read().decode("utf-8")
+    conn.close()
+    # -- Optionally you can print the
+    # -- received json file for testing
+    # print(text_json)
 
-# -- Print the status
-print()
-print("Response received: ", end='')
-print(r1.status, r1.reason)
+    # -- Generate the object from the json file
+    weather = json.loads(text_json)
 
-# -- Read the response's body and close
-# -- the connection
-text_json = r1.read().decode("utf-8")
-conn.close()
+    # -- Get the data
+    time = weather['time']
 
-# -- Optionally you can print the
-# -- received json file for testing
-# print(text_json)
+    temp0 = weather['consolidated_weather'][0]
+    description = temp0['weather_state_name']
+    temp = temp0['the_temp']
+    place = weather['title']
 
-# -- Generate the object from the json file
-weather = json.loads(text_json)
-
-# -- Get the data
-time = weather['time']
-
-temp0 = weather['consolidated_weather'][0]
-description = temp0['weather_state_name']
-temp = temp0['the_temp']
-place = weather['title']
-
-print()
-print("Place: {}".format(place))
-print("Time: {}".format(time))
-print("Weather description: {}".format(description))
-print("Current temp: {} degrees".format(temp))
+    print()
+    print("Place: {}".format(place))
+    print("Time: {}".format(time))
+    print("Weather description: {}".format(description))
+    print("Current temp: {} degrees".format(temp))
